772e82c754ca63bfb7c308a10aeba594aefdd403
natiist0 <128399846+natiist0@users.noreply.github.com>
2023-06-22 06:04:36 -0400

    EXAMEN FINAL
    me falto validar el formulario con js

--- examen/natiist0/script.js
+++ examen/natiist0/script.js
@@ -1,2 +1,4 @@
 let nombre = prompt ("Dime tu nombre")
 alert ("Hola " + nombre + ", En esta pagina web encontraras nuevas recomendaciones de canciones cada semana, espero que lo disfrutes ^_^ ")
+
+
