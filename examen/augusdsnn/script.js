e40329f78c7881ca9becf240c56d5358654c3bff
augusdsnn <128400710+augusdsnn@users.noreply.github.com>
2023-06-22 18:16:33 -0400

    Add files via upload

---
+++ examen/augusdsnn/script.js
@@ -0,0 +1,25 @@
+//Javascript aplicado en conjunto con ChatGPT//
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var windowHeight = window.innerHeight;
+
+    var colors = ['#EFF2FF', '#dbacdc', '#ac9ac8', '#ecc2a2'];
+    var colorIndex = Math.floor(scrollPosition / 1000);
+    var colorStart = colors[colorIndex];
+    var colorEnd = colors[colorIndex + 1] || colors[colors.length - 1];
+    var progress = (scrollPosition % 1000) / 1000; 
+
+    var redStart = parseInt(colorStart.slice(1, 3), 16);
+    var greenStart = parseInt(colorStart.slice(3, 5), 16);
+    var blueStart = parseInt(colorStart.slice(5, 7), 16);
+
+    var redEnd = parseInt(colorEnd.slice(1, 3), 16);
+    var greenEnd = parseInt(colorEnd.slice(3, 5), 16);
+    var blueEnd = parseInt(colorEnd.slice(5, 7), 16);
+
+    var red = Math.round(redStart + (redEnd - redStart) * progress);
+    var green = Math.round(greenStart + (greenEnd - greenStart) * progress);
+    var blue = Math.round(blueStart + (blueEnd - blueStart) * progress);
+
+    document.body.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+});
